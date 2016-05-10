cef60f5e734edad2c9d713e33d85b585ce2b2563
Phil Leggetter <phil@leggetter.co.uk>
2016-05-10 18:35:39 +0300

    Fix some missing IdeaComment -> CommentModel

---
+++ realtime-idea-space/Config.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace realtime_idea_space
+{
+    public static class Config
+    {
+        public static string CountryCode {
+            get
+            {
+                return WebConfigurationManager.AppSettings["Nexmo.BuyNumberCountry"] ?? "GB";
+            }
+        }
+    }
+}
